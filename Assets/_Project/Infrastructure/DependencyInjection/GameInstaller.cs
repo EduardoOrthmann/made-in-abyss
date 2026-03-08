c32a1fb8807288914baef42a21379455bff50d5f
Eduardo <orthmann2811@gmail.com>
2026-03-08 19:51:26 -0300

    add paused menu background

--- Assets/_Project/Infrastructure/DependencyInjection/GameInstaller.cs
+++ Assets/_Project/Infrastructure/DependencyInjection/GameInstaller.cs
@@ -1,45 +1,12 @@
-﻿using _Project.Application.Commands;
-using Zenject;
-using UnityEngine;
-using _Project.Application.Interfaces;
-using _Project.Application.Events;
-using _Project.Application.States.GameState;
-using _Project.Infrastructure.Adapters;
-using UnityEngine.InputSystem;
+﻿using Zenject;
 
 namespace _Project.Infrastructure.DependencyInjection
 {
     public class GameInstaller : MonoInstaller
     {
-        [SerializeField] private GameStateEventChannel gameStateEventChannel;
-        [SerializeField] private TransitionEventChannel transitionEventChannel;
-        [SerializeField] private InputActionReference pauseInputAction;
-
         public override void InstallBindings()
         {
-            // Event Channels
-            Container.BindInstance(gameStateEventChannel).AsSingle();
-            Container.BindInstance(transitionEventChannel).AsSingle();
-
-            // Services
-            Container.Bind<ITimeService>().To<UnityTimeAdapter>().AsSingle();
-            Container.Bind<ISceneLoader>().To<UnitySceneLoader>().AsSingle();
-
-            // Commands
-            Container.Bind<CommandProcessor>().AsSingle();
-            Container.BindFactory<Domain.ScriptableObjects.LevelData, System.Action, LoadLevelCommand, LoadLevelCommand.Factory>().AsSingle();
-            Container.BindFactory<Domain.ScriptableObjects.LevelData, System.Action, UnloadLevelCommand, UnloadLevelCommand.Factory>().AsSingle();
-
-            // States
-            Container.Bind<IGameState>().To<BootstrapState>().AsSingle();
-            Container.Bind<IGameState>().To<MainMenuState>().AsSingle();
-            Container.Bind<IGameState>().To<PlayingState>().AsSingle();
-            Container.Bind<IGameState>().To<PausedState>().AsSingle();
-
-            Container.Bind<IGameStateMachine>().To<GameStateMachine>().AsSingle();
-
-            Container.BindInstance(pauseInputAction).WhenInjectedInto<InputAdapter>();
-            Container.BindInterfacesTo<InputAdapter>().AsSingle();
+            // MainScene specific dependencies like MainSceneAudioOrchestrator for exemple or specific UIManager
         }
     }
 }
--- Assets/_Project/Infrastructure/DependencyInjection/ProjectInstaller.cs
+++ Assets/_Project/Infrastructure/DependencyInjection/ProjectInstaller.cs
@@ -1,12 +1,46 @@
 ﻿using Zenject;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using _Project.Application.Commands;
+using _Project.Application.Events;
+using _Project.Application.Interfaces;
+using _Project.Application.States.GameState;
+using _Project.Infrastructure.Adapters;
 
 namespace _Project.Infrastructure.DependencyInjection
 {
     public class ProjectInstaller : MonoInstaller
     {
+        [SerializeField] private GameStateEventChannel gameStateEventChannel;
+        [SerializeField] private TransitionEventChannel transitionEventChannel;
+        [SerializeField] private InputActionReference pauseInputAction;
+
         public override void InstallBindings()
         {
-            // Global bindings
+            // Global Event Channels
+            Container.BindInstance(gameStateEventChannel).AsSingle();
+            Container.BindInstance(transitionEventChannel).AsSingle();
+
+            // Core Services
+            Container.Bind<ITimeService>().To<UnityTimeAdapter>().AsSingle();
+            Container.Bind<ISceneLoader>().To<UnitySceneLoader>().AsSingle();
+
+            // Commands
+            Container.Bind<CommandProcessor>().AsSingle();
+            Container.BindFactory<Domain.ScriptableObjects.LevelData, System.Action, LoadLevelCommand, LoadLevelCommand.Factory>().AsSingle();
+            Container.BindFactory<Domain.ScriptableObjects.LevelData, System.Action, UnloadLevelCommand, UnloadLevelCommand.Factory>().AsSingle();
+
+            // Game State Machine
+            Container.Bind<IGameState>().To<BootstrapState>().AsSingle();
+            Container.Bind<IGameState>().To<MainMenuState>().AsSingle();
+            Container.Bind<IGameState>().To<PlayingState>().AsSingle();
+            Container.Bind<IGameState>().To<PausedState>().AsSingle();
+
+            Container.Bind<IGameStateMachine>().To<GameStateMachine>().AsSingle();
+
+            // Input
+            Container.BindInstance(pauseInputAction).WhenInjectedInto<InputAdapter>();
+            Container.BindInterfacesTo<InputAdapter>().AsSingle();
         }
     }
 }
